9d617966b9fd4dc58219c96fd63c2c245f850ea0
DarkComicHero <seven@darkcomichero.com>
2018-02-08 20:59:07 -0600

    edit
    edit from https://forum.fivem.net/t/release-vrp-sell-drugs-to-npc/68728/35

---
+++ config.lua
@@ -0,0 +1,5 @@
+cfg = {}
+
+--Discord Webhook up in your discord.
+cfg.e911_webhook = "https://discordapp.com/api/webhooks/411343886196277259/MpL9LT6kbbwUhFoAtJF1U49UWFvJ2AxlUd0pLXoAUz--0xaPwGl2A2D3LrBxBYvQEOBV"; 
+return cfg
---
+++ server.lua
@@ -0,0 +1,45 @@
+local Proxy = module("vrp", "lib/Proxy")
+local Tunnel = module("vrp", "lib/Tunnel")
+
+local cfg = module("vrp_drugstonpc", "config")
+
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP","vrp_kekmememememes")
+
+RegisterServerEvent('drugs:item')
+AddEventHandler('drugs:item', function()
+	local user_id = vRP.getUserId({source})
+	local player = vRP.getUserSource({user_id})
+	if not vRP.tryGetInventoryItem({user_id,"weed",1,notify}) then
+		TriggerClientEvent('done', player)
+		TriggerClientEvent('cancel', player)
+	else
+		TriggerClientEvent('cancel', player)
+	end
+end)
+
+RegisterServerEvent('vRP_drugNPC:police1')
+AddEventHandler('vRP_drugNPC:police1', function(x,y,z,street1,tipResponse)
+     vRP.sendServiceAlert({nil, "police",x,y,z,tipResponse .. " on " .. street1 .. "."})
+	 sendToDiscord('TS Tipline', tipResponse .. ' on ' .. street1 .. '.', cfg.e911_webhook)
+end)
+
+RegisterServerEvent('vRP_drugNPC:police2')
+AddEventHandler('vRP_drugNPC:police2', function(x,y,z,street1,street2,tipResponse)
+     vRP.sendServiceAlert({nil, "police",x,y,z,"TIPLINE | " .. tipResponse .. " between " .. street1 .. " and " .. street2.. "."})
+	 sendToDiscord('TS TIPLINE | ', tipResponse .. ' between ' .. street1 .. ' and ' .. street2.. '.', cfg.e911_webhook)
+end)
+
+RegisterServerEvent('drugs:money')
+AddEventHandler('drugs:money', function()
+	local user_id = vRP.getUserId({source})
+	local player = vRP.getUserSource({user_id})
+	local cant = math.random(200,600)
+	vRP.giveInventoryItem({user_id,"dirty_money",cant,notify})
+end)
+
+function sendToDiscord(name, message, webhook)
+print('sending to discord...'..webhook)
+  if message == nil or message == '' then return FALSE end
+  PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = name, content = message, tts = true}), { ['Content-Type'] = 'application/json' })
+end
